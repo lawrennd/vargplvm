39e82f3ced976d403e834e5bb7aec99a8ca6d593
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2009-09-30 11:05:20 +0000

    Changes to way model files are written to save space.

---
+++ matlab/vargplvmToolboxes.m
@@ -0,0 +1,11 @@
+% VARGPLVMTOOLBOXES Load in the relevant toolboxes for variational gplvm.
+
+importLatest('netlab');
+importLatest('prior');
+importLatest('optimi');
+importLatest('datasets');
+importLatest('mltools');
+importTool('kern');
+importLatest('ndlutil');
+importLatest('mocap');
+importLatest('gp');
