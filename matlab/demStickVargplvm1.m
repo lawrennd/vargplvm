f772bd68df325759b24d1f9bc8ddae5d03078441
lawrennd <lawrennd@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2011-02-08 16:48:46 +0000

    Stick man demo for vargplvm.

---
+++ matlab/demStickVargplvm1.m
@@ -0,0 +1,51 @@
+% DEMSTICKVARGPLVM1 Run variational GPLVM on stick man data.
+
+% VARGPLVM
+
+% Fix seeds
+randn('seed', 1e5);
+rand('seed', 1e5);
+
+dataSetName = 'stick';
+experimentNo = 1;
+printDiagram = 1;
+
+% load data
+[Y, lbls] = lvmLoadData(dataSetName);
+
+% Set up model
+options = vargplvmOptions('dtcvar');
+options.kern = {'rbfard2', 'bias', 'white'};
+options.numActive = 50; 
+%options.tieParam = 'tied';  
+
+options.optimiser = 'scg';
+latentDim = 10;
+d = size(Y, 2);
+
+% demo using the variational inference method for the gplvm model
+model = vargplvmCreate(latentDim, d, Y, options);
+%
+model = vargplvmParamInit(model, model.m, model.X); 
+model.vardist.covars = 0.5*ones(size(model.vardist.covars)) + 0.001*randn(size(model.vardist.covars));
+%model.vardist.covars = 0.2*ones(size(model.vardist.covars));
+
+% Optimise the model.
+iters = 2000;
+display = 1;
+
+model = vargplvmOptimise(model, display, iters);
+
+% Save the results.
+modelWriteResult(model, dataSetName, experimentNo);
+
+if exist('printDiagram') & printDiagram
+  lvmPrintPlot(model, lbls, dataSetName, experimentNo);
+end
+
+% load connectivity matrix
+[void, connect] = mocapLoadTextData('run1');
+% Load the results and display dynamically.
+lvmResultsDynamic(model.type, dataSetName, experimentNo, 'stick', connect)
+
+
--- matlab/readme.txt
+++ matlab/readme.txt
@@ -1,6 +1,11 @@
 The vargplvm toolbox is an implementation of the variational approximation for the Bayesian GPLVM.
 
 
+Version 0.11
+------------
+
+Second version with improved numerical stability implemented by Andreas Damianou and Michalis Titsias. 
+
 Version 0.1
 -----------
 
