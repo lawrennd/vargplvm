a4f320d9486988218b475a7bca9a8bef97ea6ce1
Andreas Damianou <adamianou@gmail.com>
2011-06-02 11:59:48 +0000

    deleted codeNIPS folder

--- codeNIPS/VGPDS/matlab/modelPriorKernGrad.m
+++
@@ -1,8 +0,0 @@
-function [muqOrig SqOrig] = modelPriorKernGrad(dynModel)
-
-fhandle = str2func([dynModel.type 'PriorKernGrad']);
-[muqOrig SqOrig] = fhandle(dynModel);
-
-% if isfield(model, 'paramGroups')
-%  g = g*model.paramGroups;
-% end
--- codeNIPS/VGPDS/matlab/readme.txt
+++
@@ -1,7 +0,0 @@
-The vargplvm toolbox is an implementation of the variational approximation for the Bayesian GPLVM.
-
-
-Version 0.1
------------
-
-First version of the software with implementation of the 2010 AISTATS paper.
