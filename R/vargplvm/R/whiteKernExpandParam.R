b755864ef5b3a01eb2863474cd227f3f076c7930
jhao <jhao@c7ad7044-cb60-498d-a845-55a1d3ae732a>
2012-06-19 16:30:46 +0000

    remove test

--- R/vargplvm/R/whiteKernExpandParam.R
+++
@@ -1,9 +0,0 @@
-whiteKernExpandParam <-
-function (kern, params) {
-  if ( is.list(params) )
-    params <- params$values
-
-  kern$variance <- params[1]	## linear domain param, i.e. the untransformed noise variance
-
-  return (kern)
-}
